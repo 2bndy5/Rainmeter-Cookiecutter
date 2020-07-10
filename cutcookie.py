da45c2a4985e0cb2067f4d3e6fd5d72451cd8ad9
brendan <2bndy5@gmail.com>
2020-07-10 14:04:26 -0700

    this works now

--- cutcookie.py
+++ cutcookie.py
@@ -8,16 +8,59 @@ import winreg
 import configparser
 import json
 from cookiecutter.main import cookiecutter
+from cookiecutter import prompt
 
 defaults = {
-    "req_rainmeter_version": "4.3",
     "author_full_name": os.getenv("USERNAME"),
-    "_skins_path": "",
-    "_layouts": {},
-    "_layout_path": "",
-    "_installed": {"Skin": ["Create a new skin"], "Layout": []},
+    "github_username": "my.github.account",
+    "req_rainmeter_version": "4.3",
     "_year": time.strftime("%Y", time.localtime()),
+    "_skins_path": "",
+    "_layouts_path": "",
+    "installed": {"Skin": ["Create a new skin"], "Layout": []},
+    "import_type": ["Skin", "Layout"],
+    "import": [],
+    "_load_skin": "NONE",
+    "project_name": "",
+    "repository_name": "",
+    "short_description": "A collection of my skins for Rainmeter",
+    "license": ["MIT", "CC BY-SA", "GNU GPLv3"],
+    "_copy_without_render": ["*.github/*"],
 }
+
+
+def list_to_string(data, indent=0):
+    result = ""
+    for item in data:
+        if isinstance(item, dict):
+            result += dict_to_str(item, indent + 1)
+        elif isinstance(item, list):
+            if len(item) > 1:
+                result += list_to_string(item, indent + 1)
+            else:
+                result += " " * 2 * indent + '- "' + item[0] + '"\n'
+        else:
+            result += " " * 2 * indent + '- "' + item + '"\n'
+    return result
+
+
+def dict_to_str(data, indent=0):
+    result = ""
+    for k, v in data.items():
+        if isinstance(v, dict):
+            result += " " * 2 * indent + k.replace(" ", "_") + ":\n"
+            result += dict_to_str(v, indent + 1)
+        elif isinstance(v, list):
+            result += " " * 2 * indent + k.replace(" ", "_") + ":\n"
+            result += list_to_string(v, indent + 1)
+        else:
+            result += " " * 2 * indent + k.replace(" ", "_") + ': "' + v + '"\n'
+    return result
+
+
+def dump_to_yml(data_dict, filename):
+    with open(filename + ".yml", "w") as file:
+        file.write(dict_to_str(data_dict))
 
 
 def read_config(file_path):
@@ -31,73 +74,103 @@ def read_config(file_path):
         return parser
 
 
-rm_reg_key = None
-try:
-    rm_reg_key = winreg.OpenKey(
-        winreg.HKEY_LOCAL_MACHINE,
-        "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Rainmeter",
-    )
-except FileNotFoundError:
-    print("Rainmeter is not installed!")
-
-
 def main():
     """Gathers default data before prompting user for input."""
+    rm_reg_key = None
+    try:
+        rm_reg_key = winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Rainmeter",
+        )
+    except FileNotFoundError:
+        print("Rainmeter is not installed!")
     if rm_reg_key is not None:
         defaults["req_rainmeter_version"] = winreg.QueryValueEx(
             rm_reg_key, "DisplayVersion"
+        )[0].replace(" r", ".")
+        defaults["_layouts_path"] = (
+            os.getenv("APPDATA") + os.sep + "Rainmeter\\Layouts"
         )
-        defaults["req_rainmeter_version"] = defaults["req_rainmeter_version"][
-            0
-        ].replace(" r", ".")
-        defaults["_layout_path"] = os.getenv("APPDATA") + os.sep + "Rainmeter\\Layouts"
 
         # get path to Rainmeter skins
         parsed = read_config(os.getenv("APPDATA") + "\\Rainmeter\\Rainmeter.ini")
         if "Rainmeter" in parsed and "SkinPath" in parsed["Rainmeter"]:
             if parsed["Rainmeter"]["SkinPath"].endswith(os.sep):
                 # remove trailing path seperator
-                defaults["_skins_path"] = parsed["Rainmeter"]["SkinPath"][:-1]
+                defaults["_skins_path"] = parsed["Rainmeter"][
+                    "SkinPath"
+                ][:-1]
             else:
-                defaults["_skins_path"] = parsed["Rainmeter"]["SkinPath"]
+                defaults["_skins_path"] = parsed["Rainmeter"][
+                    "SkinPath"
+                ]
 
         # grab names of installed skins
         if len(defaults["_skins_path"]):
             for d in os.listdir(defaults["_skins_path"]):
                 if not d.startswith("@"):
                     # ignore @Backup & @Vault folders
-                    defaults["_installed"]["Skin"].append(d)
+                    defaults["installed"]["Skin"].append(d)
 
         # now collect layouts
-        for d in os.listdir(defaults["_layout_path"]):
-            # read layout files in subfolders
-            parsed = read_config(
-                defaults["_layout_path"] + os.sep + d + os.sep + "Rainmeter.ini"
-            )
-            needed_skins = []
-            for key in parsed.keys():
-                # exclude inactive skins & [Rainmeter] section
-                if "Active" in parsed[key] and bool(int(parsed[key]["Active"])):
-                    # only grab root config name as a necessary skin for layout
-                    root_config = key.split(os.sep)[0]
-                    if not root_config in needed_skins:
-                        # avoid duplicate entries
-                        needed_skins.append(root_config)
-            defaults["_layouts"][d] = needed_skins
-            defaults["_installed"]["Layout"].append(d)
+        for d in os.listdir(defaults["_layouts_path"]):
+            defaults["installed"]["Layout"].append(d)
+
+    # Prompt user for input
+    for key, value in defaults.items():
+        if not key.startswith("_"):
+            if isinstance(value, list):
+                defaults[key] = prompt.read_user_choice(key, value)
+            elif isinstance(value, str):
+                defaults[key] = prompt.read_user_variable(key, value)
+            if key.endswith("import_type"):
+                defaults["import"] = defaults[
+                    "installed"
+                ][defaults[key]]
+            elif key.endswith("import"):
+                defaults["project_name"] = defaults[key]
+                defaults["repository_name"] = (
+                    defaults[key].replace(" ", "_")
+                    + "_Rainmeter_Skin"
+                )
+                # promt user to pick skin to load if importing an installed skin
+                if defaults["import_type"] == "Skin" and defaults["import"] != "Create a new skin":
+                    # now ask the user which skin to load on-install
+                    skin_configs = []
+                    for dirpath, _, filenames in os.walk(
+                        defaults["_skins_path"]
+                        + os.sep
+                        + defaults[key]
+                    ):
+                        for f in filenames:
+                            if f.endswith(".ini"):
+                                skin_configs.append(
+                                    dirpath.replace(
+                                        defaults["_skins_path"]
+                                        + os.sep,
+                                        "",
+                                    )
+                                    + os.sep
+                                    + f
+                                )
+                    defaults["_load_skin"] = prompt.read_user_choice(
+                        "skin to load", skin_configs
+                    )
+            elif key.endswith("project_name"):
+                defaults["repository_name"] = (
+                    defaults[key].replace(" ", "_")
+                    + "_Rainmeter_Skin"
+                )
+                
 
     # NOTE debugging output functions
     # dump to JSON
     with open("defaults.json", "w") as file:
         json.dump(defaults, file, indent=4)
 
-    # print to console
-    # for k, v in defaults.items():
-    #     print("{} = {}".format(k, v))
-
 
 if __name__ == "__main__":
     main()
     # now merge discovered defaults and begin cookie-cutting
-    cookiecutter(".", output_dir="..", extra_context=defaults)
+    cookiecutter(".", output_dir="..", extra_context=defaults, no_input=True)
 
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,11 @@
 import os
 import shutil
+import configparser
 
+import_type = "{{ cookiecutter.import_type }}"
 importing = "{{ cookiecutter.import }}"
 rm_skins_path = r"{{ cookiecutter._skins_path }}"
 rm_layouts_path = r"{{ cookiecutter._layouts_path }}"
-import_type = "{{ cookiecutter.import_type }}"
 
 def remove_blank_skin():
     for dirpath, dirnames, filenames in os.walk(os.getcwd() + os.sep + "Skins", topdown=False):
@@ -14,10 +15,39 @@ def remove_blank_skin():
             os.rmdir(dirpath + os.sep + d)
     
 def import_skin(root_config):
-    print("importing", root_config)
+    print(f"importing {root_config} skin")
     new_dir = "Skins" + os.sep + root_config
     skin_root = rm_skins_path + os.sep + root_config
     shutil.copytree(skin_root, new_dir)
+
+def read_config(file_path):
+    parser = configparser.ConfigParser(default_section="Rainmeter")
+    try:  # trys to open file with default utf-8 encoding
+        parser.read(file_path)
+    except configparser.MissingSectionHeaderError:
+        # This exception on this file likely means encoding is utf-16
+        parser.read(file_path, encoding="utf-16")
+    finally:
+        return parser
+
+def gather_layout_deps(layout_name):
+    parsed = read_config(
+        rm_layouts_path
+        + os.sep
+        + layout_name
+        + os.sep
+        + "Rainmeter.ini"
+    )
+    needed_skins = []
+    for key in parsed.keys():
+        # exclude inactive skins & [Rainmeter] section
+        if "Active" in parsed[key] and bool(int(parsed[key]["Active"])):
+            # only grab root config name as a necessary skin for layout
+            root_config = key.split(os.sep)[0]
+            if not root_config in needed_skins:
+                # avoid duplicate entries
+                needed_skins.append(root_config)
+    return needed_skins
 
 if import_type == 'Skin':
     # ``importing`` is a str
@@ -27,5 +57,13 @@ if import_type == 'Skin':
 elif import_type == 'Layout':
     # ``importing`` is a list
     remove_blank_skin()
-    for name in importing:
+    # needed_skins = 
+    for name in gather_layout_deps(importing):
         import_skin(name)
+    # copy layout file
+    print(f"importing {importing} layout")
+    dst = "Layouts" + os.sep + importing
+    os.mkdir("Layouts")
+    os.mkdir(dst)
+    src = rm_layouts_path + os.sep + importing + os.sep + "Rainmeter.ini"
+    shutil.copy2(src, dst)
